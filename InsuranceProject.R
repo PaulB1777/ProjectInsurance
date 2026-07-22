0137469ce654eaaaadeef1de575b790c7dce86d6
PaulB1777 <paul.br106@gmail.com>
2026-07-22 08:40:24 -0500

    Removed unwanted variables

--- InsuranceProject.R
+++ InsuranceProject.R
@@ -7,3 +7,14 @@ library(tidyr)
 insurance <- read_csv("C:/Users/paulb/OneDrive/Desktop/Projects/Insurance Project/AutoInsurance.csv")
 head(insurance)
 dim(insurance)
+
+summary(insurance)
+str(insurance)
+
+# Checking for NA values
+colSums(is.na(insurance))
+
+#remove unnecessary columns
+insurance[, 
+      c("Customer", "Customer Lifetime Value", "Effective To Date", "Monthly Premium Auto", "Months Since Last Claim", "Months Since Policy Inception", "Number of Open Complaints", "Number of Policies", "Renew Offer Type", "Sales Channel", "Policy", "Policy Type")
+] <- list(NULL)
