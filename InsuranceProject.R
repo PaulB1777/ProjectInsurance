62a6bea2396a6eb1c4aeb2463ced8f758749bbb1
PaulB1777 <paul.br106@gmail.com>
2026-07-23 09:46:51 -0500

    Creating Visuals and summary statistics

--- InsuranceProject.R
+++ InsuranceProject.R
@@ -18,3 +18,35 @@ colSums(is.na(insurance))
 insurance[, 
       c("Customer", "Customer Lifetime Value", "Effective To Date", "Monthly Premium Auto", "Months Since Last Claim", "Months Since Policy Inception", "Number of Open Complaints", "Number of Policies", "Renew Offer Type", "Sales Channel", "Policy", "Policy Type")
 ] <- list(NULL)
+
+claimsummary <- summary(insurance$`Total Claim Amount`)
+claimvar <- var(insurance$`Total Claim Amount`)
+print(claimsummary)
+print(claimvar)
+
+#Creating a histogram of the Total Claim Amount Variable
+ggplot(insurance, aes(x = `Total Claim Amount`)) + 
+  geom_histogram(aes(y = after_stat(density)),
+                 fill = "lightgreen", col = "black",) +
+  geom_density(
+    aes(color = "Density Curve"), 
+    lwd = 0.7) +
+  stat_function(fun=dnorm, 
+                args = list(mean = claimsummary["Mean"], sd = sqrt(claimvar)),
+                aes(color = "Normal Curve"),
+                lwd = 0.7) +
+  labs(
+    title = "Distribution of Auto Claim Amounts",
+    x = "Claim Amount",
+    y = "Density",
+    color = "Legend"
+  )
+
+# Creating a box plot
+ggplot(insurance, aes(y = "", x = `Total Claim Amount`)) +
+  stat_boxplot(geom="errorbar") +
+  geom_boxplot(fill="lightgreen") +
+  stat_summary(fun = mean, col = "black", geom = "point", size = 3) +
+  xlab ("Claim Amount") +
+  ylab ("") +
+  labs(title = "Total Claim Amounts")
