5db73af3e88378bbd14183913ff654f546bd20d4
PaulB1777 <paul.br106@gmail.com>
2026-07-25 11:06:22 -0500

    Update InsuranceProject.R

--- InsuranceProject.R
+++ InsuranceProject.R
@@ -16,7 +16,7 @@ colSums(is.na(insurance))
 
 #remove unnecessary columns
 insurance[, 
-      c("Customer", "Customer Lifetime Value", "Effective To Date", "Monthly Premium Auto", "Months Since Last Claim", "Months Since Policy Inception", "Number of Open Complaints", "Number of Policies", "Renew Offer Type", "Sales Channel", "Policy", "Policy Type")
+      c("Customer", "Customer Lifetime Value", "Effective To Date", "Response", "Coverage", "Monthly Premium Auto", "Months Since Last Claim", "Months Since Policy Inception", "Number of Open Complaints", "Number of Policies", "Renew Offer Type", "Sales Channel", "Policy", "Policy Type")
 ] <- list(NULL)
 
 claimsummary <- summary(insurance$`Total Claim Amount`)
@@ -50,3 +50,83 @@ ggplot(insurance, aes(y = "", x = `Total Claim Amount`)) +
   xlab ("Claim Amount") +
   ylab ("") +
   labs(title = "Total Claim Amounts")
+
+# Gender Analysis
+
+#Analyzing
+tapply(insurance$'Total Claim Amount', insurance$Education, mean)
+tapply(insurance$'Total Claim Amount', insurance$Gender, median)
+tapply(insurance$'Total Claim Amount', insurance$Gender, sd)
+
+#Boxplot
+ggplot(insurance, 
+       aes(
+         x = Gender, 
+         y = `Total Claim Amount`)) +
+  coord_flip() +
+  stat_boxplot(geom="errorbar") +
+  geom_boxplot(fill="lightgreen") +
+  stat_summary(fun = mean, col = "black", geom = "point", size = 3) +
+  xlab ("Gender") +
+  ylab ("Claim Amount") +
+  labs(title = "Gender vs. Claim Amount")
+
+# Education Analysis
+tapply(insurance$'Total Claim Amount', insurance$Education, mean)
+tapply(insurance$'Total Claim Amount', insurance$Education, median)
+tapply(insurance$'Total Claim Amount', insurance$Education, sd)
+tapply(insurance$'Total Claim Amount', insurance$Education, length)
+
+ggplot(insurance, 
+       aes(
+         x = reorder(Education, `Total Claim Amount`, FUN = mean), 
+         y = `Total Claim Amount`)) +
+  coord_flip() +
+  stat_boxplot(geom="errorbar") +
+  geom_boxplot(fill="lightgreen") +
+  stat_summary(fun = mean, col = "black", geom = "point", size = 3) +
+  xlab ("Education Level") +
+  ylab ("Claim Amount") +
+  labs(title = "Education Level vs. Claim Amount")
+
+# Employment Analysis
+tapply(insurance$'Total Claim Amount', insurance$EmploymentStatus, mean)
+tapply(insurance$'Total Claim Amount', insurance$EmploymentStatus, median)
+tapply(insurance$'Total Claim Amount', insurance$EmploymentStatus, sd)
+tapply(insurance$'Total Claim Amount', insurance$EmploymentStatus, length)
+
+ggplot(insurance, 
+       aes(
+         x = reorder(EmploymentStatus, `Total Claim Amount`, FUN = mean), 
+         y = `Total Claim Amount`)) +
+  coord_flip() +
+  stat_boxplot(geom="errorbar") +
+  geom_boxplot(fill="lightgreen") +
+  stat_summary(fun = mean, col = "black", geom = "point", size = 3) +
+  xlab ("Education Level") +
+  ylab ("Claim Amount") +
+  labs(title = "Education Level vs. Claim Amount")
+
+# Income
+ggplot(insurance, aes(x = Income, y = `Total Claim Amount`)) +
+  geom_point() +
+  geom_smooth(method = lm, se = FALSE) +
+  ggtitle("Claim Size vs. Income") 
+
+# Marital Status
+tapply(insurance$'Total Claim Amount', insurance$`Marital Status`, mean)
+tapply(insurance$'Total Claim Amount', insurance$`Marital Status`, median)
+tapply(insurance$'Total Claim Amount', insurance$`Marital Status`, sd)
+tapply(insurance$'Total Claim Amount', insurance$`Marital Status`, length)
+
+ggplot(insurance, 
+       aes(
+         x = reorder(`Marital Status`, `Total Claim Amount`, FUN = mean), 
+         y = `Total Claim Amount`)) +
+  coord_flip() +
+  stat_boxplot(geom="errorbar") +
+  geom_boxplot(fill="lightgreen") +
+  stat_summary(fun = mean, col = "black", geom = "point", size = 3) +
+  xlab ("Education Level") +
+  ylab ("Claim Amount") +
+  labs(title = "Education Level vs. Claim Amount")
